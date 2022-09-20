6256e82dc0079d0dd0f9fa78d27066c9d96bd744
Hector Haro H <hector8.haro@gmail.com>
2022-09-20 13:40:29 -0500

    refactor NavLink component

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function BlogPage() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-    </div>
+    <>
+      <h1>BlogPage</h1>
+      <ul>
+      {blogdata.map(post => (
+        <BlogLink post={post} />
+      ))}
+      </ul>
+    </>
   );
 }
+
+function BlogLink({ post }) {
+  return (
+    <li>
+      <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    </li>
+  );
+}
+
+const blogdata = [];
+blogdata.push({
+  title: 'Que es REACT',
+  slug: 'que-es-react',
+  content: 'React es el mejor Framework',
+  author: 'Hector Haro Hermosillo'
+});
+blogdata.push({
+  title: 'Que es VUE',
+  slug: 'que-es-vue',
+  content: 'VUE es el mejor Framework',
+  author: 'Hector Haro Hermosillo'
+});
+blogdata.push({
+  title: 'Que es ANGULAR',
+  slug: 'que-es-angular',
+  content: 'ANGULAR es el mejor Framework',
+  author: 'Hector Haro Hermosillo'
+});
 
 export { BlogPage };
